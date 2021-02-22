7a5d07a127df569191e7e8c7ae48eeacfa0150d1
saitarun135 <saitarun135@gm.com>
2021-02-22 13:29:52 +0530

    adding: present or not

--- selection-statement/empWage.sh
+++ selection-statement/empWage.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
-empCheck=$((RANDOM%2))
-isPresent=1
-if [ $empCheck -eq $isPresent ]
-then 
-echo Employee is present
-else 
-echo Employee is absent
+randChk=$((RANDOM%2))
+isPresent=1;
+if [ $isPresent -eq $randChk ]
+then
+echo "employee is present"
+else
+echo "employee is absemt"
 fi
+
