91ecfc6f721930c115c46c55504ec82fba1702d0
saitarun135 <saitarun135@gm.com>
2021-02-22 18:45:34 +0530

    1st step: generating 3digit numbers

---
+++ selection-statement/maxmin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x 
+arrays=($r1 $r2 $r3 $r4 $r5)
+r1=$((RANDOM%999+100)) 
+r2=$((RANDOM%999+100))
+r3=$((RANDOM%999+100))
+r4=$((RANDOM%999+100))
+r5=$((RANDOM%999+100))
+for int in `$arrays`
+do
+if [int[] -ge 1000]
+then 
+int[]=int[]-99;
+fi
+echo $arrays
+done
+
