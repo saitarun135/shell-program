409b8682508d15a8dcf8aca99e40e954d37f35f7
saitarun135 <saitarun135@gm.com>
2021-02-23 16:22:54 +0530

    single digit to word

---
+++ casestatement/singlecase.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+echo "enter a number"
+read n
+case $n in
+0)
+echo  $n="ZERO"
+;;
+1)
+echo $n="ONE"
+;;
+2)
+echo  $n="TWO"
+;;
+3)
+echo $n="THREE"
+;;
+4)
+echo  $n="FOUR"
+;;
+5)
+echo $n="FIVE"
+;;
+6)
+echo  $n="SIX"
+;;
+7)
+echo  $n="SEVEN"
+;;
+8)
+echo $n="EIGHT"
+;;
+9)
+ECHO $N="nine"
+;;
+
+*)
+echo not
+;;
+esac
+
+
