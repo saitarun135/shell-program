5b26b702616d1fb8215396271be2330f749e1959
saitarun135 <saitarun135@gm.com>
2021-02-24 22:05:27 +0530

    magic(not_completed)

--- while-loop-statements/magic.sh
+++ while-loop-statements/magic.sh
@@ -10,23 +10,4 @@ while [ $low -ne $medium ]
 do
 	medium=$((low+(high-low)/2))
 read -p "Enter 1 if number is less than $medium or Enter 0 if number is greater than $medium :" ans
-if [ $medium -eq $low ]
-then 	if [ $ans -eq 1 ]
- then
- 			break;
-	else 
-     echo $(($medium+1))
-		break;
-fi
-fi
-
-if [ $ans -eq 1 ]
-	then
-		high=$medium
-	else
-		low=$(( $medium + 1 ))
-	fi
-done
-
-echo your selected number is $medium
 
