31f0939e0644391a28d23857e8199283ea50041b
saitarun135 <saitarun135@gm.com>
2021-02-24 13:27:32 +0530

    power of 2^n

---
+++ while-loop-statements/power.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+read -p "enter number" number
+counter=0;
+result=0;
+while (($counter <= $number && $result < 256))
+do
+ result=$((2**counter))
+echo $result
+	counter=$((counter+1))
+done
+
